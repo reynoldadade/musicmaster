31a93a306965c5f3ad3abafcb02397f9b26c95dd
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-04 16:43:19 +0000

    format search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import Artist from './Artist';
+import Tracks from './Tracks';
+import Search from './Search';
+
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 
 class App extends Component {
-  render() {
-    return <div>React App</div>;
-  }
+    state = { artist: null, tracks: [] };
+
+    componentDidMount() {
+        this.searchArtist('pentatonix');
+    }
+
+    searchArtist = artistQuery => {
+        fetch(`${API_ADDRESS}/artist/${artistQuery}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.artists.total > 0) {
+                    const artist = data.artists.items[0];
+                    this.setState({
+                        artist,
+                    });
+                    fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                        .then(response => response.json())
+                        .then(data => {
+                            this.setState({
+                                tracks: data.tracks,
+                            });
+                            console.log('tracks', data);
+                        })
+                        .catch(error => alert(error.message));
+                }
+            })
+            .catch(error => alert(error.message));
+    };
+    render() {
+        return (
+            <div>
+                {/* <h2>Music Master</h2> */}
+                <Search searchArtist={this.searchArtist} />
+                <Artist artist={this.state.artist} />
+                <Tracks tracks={this.state.tracks} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    state = { artistQuery: '' };
+    updateArtistQuery = event => {
+        this.setState({
+            artistQuery: event.target.value,
+        });
+    };
+
+    handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            this.searchArtist();
+        }
+    };
+
+    searchArtist = () => {
+        this.props.searchArtist(this.state.artistQuery);
+    };
+    render() {
+        return (
+            <div>
+                <input
+                    className="form-control"
+                    onChange={this.updateArtistQuery}
+                    onKeyPress={this.handleKeyPress}
+                    placeholder="Search for an artist"
+                />
+                <button onClick={this.searchArtist} className="btn btn-default">
+                    Search
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Search;
